1558aebd713a264becbaa6b4d0cfdc5c5655bc9c
gustavooavila <gustavooavila@gmail.com>
2023-10-24 01:47:24 -0300

    test release

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-// Simple http server
+const possible_squares = [
+    "top-left", "top-middle", "top-right",
+    "middle-left", "middle", "middle-right",
+    "bottom-left", "bottom-middle", "bottom-right"
+];
 
-const http = require('node:http');
-const path = require('node:path');
-const fs = require('node:fs');
-
-const content_root = path.resolve(__dirname, "game");
-
-content_types = {
-    "html": "text/html",
-    "js": "text/javascript",
-    "css": "text/css",
-    "png": "image/png",
-    "woff": "font/woff" 
+function moveViewTo(game_container, square) {
+    possible_squares.forEach((square) => game_container.classList.remove(square));
+    game_container.classList.add(square);
 }
 
-const server = http.createServer((req, res) => {
-    const requested_path = path.resolve(path.join(content_root, req.url));
+function resetView(game_container){
+    possible_squares.forEach((square) => game_container.classList.remove(square));
+}
 
-    if(fs.existsSync(requested_path) && fs.statSync(requested_path).isFile()) {
-        console.log(`${requested_path}`)
-        const file_extension = path.extname(requested_path).replace(".", "");
-        const file_contents = fs.readFileSync(requested_path);
-
-        const content_type = file_extension in content_types ? content_types[file_extension] : "text/plain";
-
-        res.writeHead(200, { 'Content-Type': content_type });
-        res.end(file_contents);
-    return;
-    }
-   
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end("File Not Found");
+document.addEventListener("DOMContentLoaded", () => {
+    const game_container = document.getElementsByClassName('game-container')[0];
 });
-
-server.listen(8000);
